d5549ecfb7732b3c748c660fafec5b2395a20198
Bermuda <bermuda421@gmail.com>
2023-11-13 14:57:37 +0800

    Add example binding classes

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,21 @@
   <div v-html="channel"></div>
   <h2 v-bind:id="headingId">heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+  
+  <h2 class="underline">Underlined Text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? Movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array conditional movie</h2>
+  
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out' : isSoldOut
+    }">Object conditional movie</h2>
 </template>
 
 <script>
@@ -16,6 +31,9 @@ export default {
       channel: "<b>Tutorial Hell</b>",
       headingId:"heading",
       isDisabled: true,
+      status: 'danger',
+      isPromoted: true,
+      isSoldOut: true,
     }
   }
 }
